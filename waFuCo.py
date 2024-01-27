553257008e23636c2e3c53531aa4683eb81dd345
Danya Kogan <75654428+dreginkogan@users.noreply.github.com>
2024-01-26 22:39:11 -0500

    what the fuck am i doing lmao

--- waFuCo.py
+++ waFuCo.py
@@ -1,4 +1,10 @@
+import pygame
+
+file = 'tiles.png'
+image = pygame.image.load(file)
+
 terrainDict = {
+    -1: "unknown",
     0 : "water",
     1 : "grass",
     2 : "beach",
@@ -6,8 +12,12 @@ terrainDict = {
 }
 
 class Tile:
-    def __init__(self, biome):
-        self.biome = biome
+    def __init__(self, north, east, south, west, tileSetIndex: int):
+        self.north = north
+        self.east = east
+        self.south = south
+        self.west = west
+        self.tileSetIndex = tileSetIndex
 
 def printBiomes(tile:Tile, terDict:dict): # for debugging
     """
@@ -18,6 +28,35 @@ def printBiomes(tile:Tile, terDict:dict): # for debugging
     print(f"South : {terDict[tile.south]}")
     print(f"West  : {terDict[tile.west]}")
 
-testTile = Tile(1, 1, 1, 1)
+class displayThing:
+    def __init__(self, tilesWidth, tilesHeight):
+        pygame.init()
+        self.tilesWidth = tilesWidth
+        self.tilesHeight = tilesHeight
 
-printBiomes(testTile, terrainDict)
+        self.screen = pygame.display.set_mode([tilesWidth,tilesHeight])
+
+waterTile = Tile(0, 0, 0, 0, 0)
+grassTile = Tile(1, 1, 1, 1, 1)
+beachTile = Tile(2, 2, 2, 2, 2)
+forestTile = Tile(3, 3, 3, 3, 3)
+
+printBiomes(grassTile, terrainDict)
+
+
+
+
+rect = image.get_rect()
+print(image)
+
+testThing = displayThing(500, 500)
+
+screen = testThing.screen
+
+screen.blit(image, rect)
+pygame.display.update()
+
+while True:
+   for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
