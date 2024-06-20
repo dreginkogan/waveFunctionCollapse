0cabc2ccb11d3317a9263bf9b52dc9c1dac728c4
Danya Kogan <75654428+dreginkogan@users.noreply.github.com>
2024-06-20 13:11:03 -0400

    it works

--- main.py
+++ main.py
@@ -3,8 +3,13 @@ import time
 
 # keep the world 2d array as 3d, so that each tile can also store additional information
 
+# 0 - tile
+# 1 - structure
+# 2 - resources???
+# 3 territory???
+
 if __name__ == '__main__':
-    waFuCo = WaFuCo(128, 48, 6 ,6, True)
+    waFuCo = WaFuCo(48, 64, 6 ,6, True)
 
     waFuCo.oceanBorder()
     # waFuCo.sillyTestRing(24)
